daa9fc9014598c007e2ec3a5d45d1bd2cfd8c162
Sagar Mamodia <sagar74822@gmail.com>
2025-05-19 02:01:34 +0530

    Added video upload functionality

--- video-service/main.py
+++ video-service/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, status, Form, File, Depends, UploadFile
+from fastapi import FastAPI, status, Form, File, Depends, UploadFile, Body
 from pymongo import MongoClient
-from pydantic import BaseModel
-from typing import Annotated, Union
+from pydantic import BaseModel, Field
+from typing import Annotated, Union, Dict
 from utils import JsonResponse
 from bson import ObjectId
+from gridfs import GridFS
+import uuid
 
 app = FastAPI()
 
@@ -15,6 +17,7 @@ db_videos = client['db_videos']
 upload_session = db_temp['upload_session']
 chunks = db_temp['chunks']
 
+fs = GridFS(db_videos)
 
 # Models
 class UploadSession(BaseModel):
@@ -24,7 +27,7 @@ class UploadSession(BaseModel):
     chunk_size: int
 
 class Chunk(BaseModel):
-    _id: str #upload_id_{chunk_number}
+    _id: str
     upload_id: str
     chunk_number: int
     data: bytes
@@ -33,22 +36,19 @@ class UploadSessionRequest(BaseModel):
     filename: str
     chunk_size: int
     total_chunks: int
-     
-@app.get('/home/')
-def home():
-    return JsonResponse(error='worked')
+   
 
 @app.post('/video/upload/initiate/')
 def initiate_upload(payload: UploadSessionRequest):
-    if(payload.chunk_size > 256):
-        return JsonResponse(error='Chunk size should be <= 256 bytes', status=400)
+    if(payload.chunk_size > 1024*1024*16):
+        return JsonResponse(error='Chunk size should be <= 16MB', status=400)
 
     # create an upload_session document for this request 
+    upload_session_obj = UploadSession( filename=payload.filename, chunk_size=payload.chunk_size, total_chunks=payload.total_chunks)
     try:
-        id = str(upload_session.insert_one(payload.dict()).inserted_id)
+        id = str(upload_session.insert_one(upload_session_obj.dict()).inserted_id)
     except:
         return JsonResponse(error='Failure to create upload session', status=500)
-    
     return JsonResponse(upload_session_id=id, status=200)
 
 @app.post('/video/upload/chunk/')
@@ -60,13 +60,13 @@ async def upload_chunk(
 ):
     # retrieve session 
     try:
-       session_id = upload_session.find({"_id": ObjectId(upload_id)})
+       session = upload_session.find_one({"_id": ObjectId(upload_id)})
     except:
         return JsonResponse(error="Failure to fetch upload session from database", status=500)
     
-    if(session_id is None):
+    if(session is None):
         return JsonResponse(error="Upload session does not exist", status=400)
-    
+
     try:
         file_data = await file.read()
     except:
@@ -75,7 +75,6 @@ async def upload_chunk(
     # put chunk in the database
     try:
          chunk_obj = Chunk(
-            _id=f"{upload_id}_{chunk_number}",
             upload_id=upload_id,
             chunk_number=chunk_number,
             data=file_data
@@ -89,7 +88,51 @@ async def upload_chunk(
         return JsonResponse(error="Failure to put chunk in database", status=500)
 
     return JsonResponse(result='success')
+
+@app.post('/video/upload/complete/')
+def complete_upload(payload: Dict[str, str]):
+    upload_id = payload['upload_id'] 
+    try:
+        upload_session_dict = upload_session.find_one({"_id": ObjectId(upload_id)})
+    except: 
+        return JsonResponse(error="Failure to retrieve upload session from database", status=500)  
     
+    upload_session_obj = UploadSession(**upload_session_dict)
+
+    #retrieve all the chunks 
+    binary_chunks = []
+    filename = upload_session_obj.filename
+    total_chunks = upload_session_obj.total_chunks
+
+    for i in range(total_chunks):
+        try:
+            chunk = chunks.find_one({"upload_id": upload_id, "chunk_number": i})
+        except:
+            # delete all the chunks and upload session
+            return JsonResponse(error=f"Failure to fetch chunk {i} from database", status=500)
+        
+        binary_chunks.append(chunk['data']) 
+
+    with open(filename, 'wb') as f:
+        for chunk in binary_chunks:
+            f.write(chunk)
+
+        # save this file to mongodb
+    
+    with open(filename, 'rb') as f:
+        f.seek(0)
+        try:
+            file_id = fs.put(f.read(), filename=filename)
+        except:
+            return JsonResponse(error="Failure to put merged file in database", status=500)
+            
+    with open('console.txt', 'w') as f:
+        f.write(str(file_id))
+   
+    
+    return JsonResponse(file_id=str(file_id))
+
+
 if __name__=='__main__':
     app.run(port=8000)
 
--- video-service/test.py
+++ video-service/test.py
@@ -1,15 +1,35 @@
 import requests
 import os
+import math
 
-file = open('test_file.jpg', 'rb')
-chunk = file.read(256)
+file_path = 'test_file.jpg'
+chunk_size = 1024*1024
+file_size_bytes = os.path.getsize(file_path)
+total_chunks = math.ceil(file_size_bytes / chunk_size)
 
-files = {'file': ('test_file.jpg', chunk, 'application/octet-stream')}
+# get upload id
+print(file_size_bytes, chunk_size, total_chunks)
+response = requests.post(
+    'http://localhost:8000/video/upload/initiate/',
+    json={'filename':'test_file.jpg', 'chunk_size': chunk_size, 'total_chunks': total_chunks}
+)
+
+print(response.json())
+upload_id = response.json()['upload_session_id']
+
+file = open(file_path, 'rb')
+for i in range(total_chunks):
+    chunk = file.read(chunk_size)
+    files = {'file': ('test_file.jpg', chunk, 'application/octet-stream')}
+    requests.post(
+        'http://localhost:8000/video/upload/chunk/',
+        files=files,
+        data={'upload_id': upload_id, 'filename': 'test_file.jpg', 'chunk_number': i}
+    )
 
 response = requests.post(
-    'http://localhost:8000/video/upload/chunk/',
-    files=files,
-    data={'upload_id': '682782242188c34f0369feae', 'filename': 'test_file.jpg', 'chunk_number': 1}
+    'http://localhost:8000/video/upload/complete/',
+     json = {'upload_id': upload_id}
 )
 
 print(response.json())
